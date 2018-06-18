e8000c6215531f6b5676dc8788a7350605171530
Jorge Lopez de Luna <jorgelopezdeluna@Jorges-iMac.local>
2018-06-17 18:51:41 -0700

    Initial version of the Todoey app
    Includes a basic UITableView that supports reading a hardcoded list of elements, with tap handlers and check/uncheck functionality

---
+++ Todoey/CustomTodoCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTodoCell.swift
+//  Todoey
+//
+//  Created by Jorge Lopez de Luna on 6/17/18.
+//  Copyright © 2018 Jorge Lopez de Luna. All rights reserved.
+//
+
+import UIKit
+
+class CustomTodoCell: UITableViewCell {
+
+    var taskName: String!
+    var isDone: Boolean!
+}
